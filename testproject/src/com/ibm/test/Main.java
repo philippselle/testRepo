216b489ad694124d8cb0de5227a4eab96bd87a54
Selle <Selle@IBM440-R9WNBRN.hamburg.de.ibm.com>
2016-09-12 12:18:57 +0200

    commit task TP-24

--- testproject/src/com/ibm/test/Main.java
+++ testproject/src/com/ibm/test/Main.java
@@ -6,7 +6,7 @@ public class Main {
   
   public static void main(String[] args) {
 
-    if (true) {
+    if (false) {
       System.out.println("hi");
     } else {
       System.out.println("bye");
