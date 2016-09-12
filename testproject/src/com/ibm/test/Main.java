69b1660c29c2a39108fae55c7add058742822682
Selle <Selle@IBM440-R9WNBRN.hamburg.de.ibm.com>
2016-09-12 11:55:36 +0200

    test

---
+++ testproject/src/com/ibm/test/Main.java
@@ -0,0 +1,16 @@
+package com.ibm.test;
+
+public class Main {
+  
+  public final String test = "hi";
+  
+  public static void main(String[] args) {
+
+    if (true) {
+      System.out.println("hi");
+    }
+
+
+  }
+
+}
